432c006b60b09bd481fcf7b1022ad7ed8e99ae69
Bryan <import.bryan.cosx@gmail.com>
2020-11-23 17:48:21 -0600

    Commit inicial

---
+++ app.js
@@ -0,0 +1,34 @@
+// Welcome Yargs
+
+const argv = require('./config/yargs').argv;
+
+const colors = require('colors/safe');
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        console.log('Listar');
+        listarTabla(argv.base, argv.limite);
+        // .then(lista => console.log(lista))
+        // .catch(e => console.log(e));
+        break;
+
+    case 'crear':
+        console.log('crear');
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
+
+// console.log(argv.limite);
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
